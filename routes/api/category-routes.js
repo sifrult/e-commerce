2ca25613dd9eba9d26c1d5dde2dbf53be1598dd9
Amy Snarr <amy.snarr16@gmail.com>
2023-01-30 10:28:28 -0700

    can update a category by its id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -41,8 +41,14 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+// update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.update({category_name: req.body.category_name}, {where: {id: req.params.id}})
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {

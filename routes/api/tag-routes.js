119c5250f818c53c747b15c09e0f3a6f01d407ae
Amy Snarr <amy.snarr16@gmail.com>
2023-01-30 10:56:51 -0700

    can delete tags by id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -59,8 +59,24 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+// delete on tag by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!tagData) {
+      res.status(400).json({message: 'No tag found with this id!'});
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;

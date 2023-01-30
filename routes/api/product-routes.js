2eb7583b4133748a6ad64e81c99b40080ba36341
Amy Snarr <amy.snarr16@gmail.com>
2023-01-30 11:03:06 -0700

    can find all products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,12 +1,18 @@
 const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
-// The `/api/products` endpoint
-
-// get all products
-router.get('/', (req, res) => {
-  // find all products
+// find all products
+router.get('/', async (req, res) => {
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      include: [{model: Category}, {model: Tag}]
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
 });
 
 // get one product

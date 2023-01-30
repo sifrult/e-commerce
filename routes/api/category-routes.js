2b00c3af63f046b56b10485805fb7f61ace9f5d5
Amy Snarr <amy.snarr16@gmail.com>
2023-01-30 10:35:21 -0700

    can delete a category by its id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -44,15 +44,38 @@ router.post('/', async (req, res) => {
 // update a category by its `id` value
 router.put('/:id', async (req, res) => {
   try {
-    const categoryData = await Category.update({category_name: req.body.category_name}, {where: {id: req.params.id}})
+    const categoryData = await Category.update(
+      {
+        category_name: req.body.category_name
+      },
+      {
+        where: {id: req.params.id}
+      });
+
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+// delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!categoryData) {
+      res.status(400).json({message: 'No category found with this id!'});
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;

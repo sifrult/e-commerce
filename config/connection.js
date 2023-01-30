64be9b68eaa1f096a4e1c7e736dc8424ef70ec15
Amy Snarr <amy.snarr16@gmail.com>
2023-01-30 10:08:18 -0700

    can grab data from all categories

--- config/connection.js
+++ config/connection.js
@@ -4,8 +4,8 @@ const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.MYSQL_PASSWORD, {
+      host: '127.0.0.1',
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.hasOne(Category, {
+Product.belongsTo(Category, {
   foreignKey: 'category_id',
   onDelete: 'CASCADE',
 })
